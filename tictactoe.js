b9874c8f8853c9cb3d7efb4984682ac983d13c40
Simon Vall <SimonVall00@protonmail.ch>
2020-01-23 10:19:24 +0100

    HEJ

--- tictactoe.js
+++ tictactoe.js
@@ -33,200 +33,229 @@ function hard(board) {
     let win = false;
     let block_coordinates = [undefined, undefined];
     let win_coordinates = [undefined, undefined];
+    let usedSquares = [];
     
     // The AI is 'o'
-    // Check if the AI can win
-    // Check rows
+
     for (let i = 0; i < 3; i++) {
-        if (board[i][0] == 'o' && board[i][1] == 'o' && board[i][2] == '') {
-            win = true;
-            win_coordinates[0] = i;
-            win_coordinates[1] = 2;
-            break;
+        for (let j = 0; j < 3; j++) {
+            if (board[i][j] == 'x' || board[i][j] == 'o') {
+                usedSquares.push([i, j]);
+            }
         }
-        else if (board[i][0] == 'o' && board[i][1] == '' && board[i][2] == 'o') {
-            win = true;
-            win_coordinates[0] = i;
-            win_coordinates[1] = 1;
-            break;
-        }
-        else if (board[i][0] == '' && board[i][1] == 'o' && board[i][2] == 'o') {
-            win = true;
-            win_coordinates[0] = i;
-            win_coordinates[1] = 0;
-            break;
-        }
-    }
-    // Check columns
-    for (let i = 0; i < 3; i++) {
-        if (board[0][i] == 'o' && board[1][i] == 'o' && board[2][i] == '') {
-            win = true;
-            win_coordinates[0] = 2;
-            win_coordinates[1] = i;
-            break;
-        }
-        else if (board[0][i] == 'o' && board[1][i] == '' && board[2][i] == 'o') {
-            win = true;
-            win_coordinates[0] = 1;
-            win_coordinates[1] = i;
-            break;
-        }
-        else if (board[0][i] == '' && board[1][i] == 'o' && board[2][i] == 'o') {
-            win = true;
-            win_coordinates[0] = 0;
-            win_coordinates[1] = i;
-            break;
-        }
-    }
-    // Check vertical rows
-    if (board[0][0] == 'o' && board[1][1] == 'o' && board[2][2] == '') {
-        win = true;
-        win_coordinates[0] = 2;
-        win_coordinates[1] = 2;
-    }
-    else if (board[0][0] == 'o' && board[1][1] == '' && board[2][2] == 'o') {
-        win = true;
-        win_coordinates[0] = 1;
-        win_coordinates[1] = 1;
-    }
-    else if (board[0][0] == '' && board[1][1] == 'o' && board[2][2] == 'o') {
-        win = true;
-        win_coordinates[0] = 0;
-        win_coordinates[1] = 0;
-    }
-    else if (board[0][2] == 'o' && board[1][1] == 'o' && board[2][0] == '') {
-        win = true;
-        win_coordinates[0] = 2;
-        win_coordinates[1] = 0;
-    }
-    else if (board[0][2] == 'o' && board[1][1] == '' && board[2][0] == 'o') {
-        win = true;
-        win_coordinates[0] = 1;
-        win_coordinates[1] = 1;
-    }
-    else if (board[0][2] == '' && board[1][1] == 'o' && board[2][0] == 'o') {
-        win = true;
-        win_coordinates[0] = 0;
-        win_coordinates[1] = 2;
     }
 
-    if (win) {
-        console.log("Win move")
-        move[0] = win_coordinates[0];
-        move[1] = win_coordinates[1];
+    if (usedSquares.length == 1 && usedSquares[0][0] == 0 && usedSquares[0][1] == 0) {
+        move[0] = 1;
+        move[1] = 1;
+    }
+    else if (usedSquares.length == 1 && usedSquares[0][0] == 0 && usedSquares[0][1] == 2) {
+        move[0] = 1;
+        move[1] = 1;
+    }
+    else if (usedSquares.length == 1 && usedSquares[0][0] == 2 && usedSquares[0][1] == 0) {
+        move[0] = 1;
+        move[1] = 1;
+    }
+    else if (usedSquares.length == 1 && usedSquares[0][0] == 2 && usedSquares[0][1] == 2) {
+        move[0] = 1;
+        move[1] = 1;
     }
     else {
-        // Check if the oponent needs to be blocked
+        // Check if the AI can win
         // Check rows
         for (let i = 0; i < 3; i++) {
-            if (board[i][0] == 'x' && board[i][1] == 'x' && board[i][2] == '') {
-                block = true;
-                block_coordinates[0] = i;
-                block_coordinates[1] = 2;
+            if (board[i][0] == 'o' && board[i][1] == 'o' && board[i][2] == '') {
+                win = true;
+                win_coordinates[0] = i;
+                win_coordinates[1] = 2;
                 break;
             }
-            else if (board[i][0] == 'x' && board[i][1] == '' && board[i][2] == 'x') {
-                block = true;
-                block_coordinates[0] = i;
-                block_coordinates[1] = 1;
+            else if (board[i][0] == 'o' && board[i][1] == '' && board[i][2] == 'o') {
+                win = true;
+                win_coordinates[0] = i;
+                win_coordinates[1] = 1;
                 break;
             }
-            else if (board[i][0] == '' && board[i][1] == 'x' && board[i][2] == 'x') {
-                block = true;
-                block_coordinates[0] = i;
-                block_coordinates[1] = 0;
+            else if (board[i][0] == '' && board[i][1] == 'o' && board[i][2] == 'o') {
+                win = true;
+                win_coordinates[0] = i;
+                win_coordinates[1] = 0;
                 break;
             }
         }
         // Check columns
         for (let i = 0; i < 3; i++) {
-            if (board[0][i] == 'x' && board[1][i] == 'x' && board[2][i] == '') {
-                block = true;
-                block_coordinates[0] = 2;
-                block_coordinates[1] = i;
+            if (board[0][i] == 'o' && board[1][i] == 'o' && board[2][i] == '') {
+                win = true;
+                win_coordinates[0] = 2;
+                win_coordinates[1] = i;
                 break;
             }
-            else if (board[0][i] == 'x' && board[1][i] == '' && board[2][i] == 'x') {
-                block = true;
-                block_coordinates[0] = 1;
-                block_coordinates[1] = i;
+            else if (board[0][i] == 'o' && board[1][i] == '' && board[2][i] == 'o') {
+                win = true;
+                win_coordinates[0] = 1;
+                win_coordinates[1] = i;
                 break;
             }
-            else if (board[0][i] == '' && board[1][i] == 'x' && board[2][i] == 'x') {
-                block = true;
-                block_coordinates[0] = 0;
-                block_coordinates[1] = i;
+            else if (board[0][i] == '' && board[1][i] == 'o' && board[2][i] == 'o') {
+                win = true;
+                win_coordinates[0] = 0;
+                win_coordinates[1] = i;
                 break;
             }
         }
         // Check vertical rows
-        if (board[0][0] == 'x' && board[1][1] == 'x' && board[2][2] == '') {
-            block = true;
-            block_coordinates[0] = 2;
-            block_coordinates[1] = 2;
+        if (board[0][0] == 'o' && board[1][1] == 'o' && board[2][2] == '') {
+            win = true;
+            win_coordinates[0] = 2;
+            win_coordinates[1] = 2;
         }
-        else if (board[0][0] == 'x' && board[1][1] == '' && board[2][2] == 'x') {
-            block = true;
-            block_coordinates[0] = 1;
-            block_coordinates[1] = 1;
+        else if (board[0][0] == 'o' && board[1][1] == '' && board[2][2] == 'o') {
+            win = true;
+            win_coordinates[0] = 1;
+            win_coordinates[1] = 1;
         }
-        else if (board[0][0] == '' && board[1][1] == 'x' && board[2][2] == 'x') {
-            block = true;
-            block_coordinates[0] = 0;
-            block_coordinates[1] = 0;
+        else if (board[0][0] == '' && board[1][1] == 'o' && board[2][2] == 'o') {
+            win = true;
+            win_coordinates[0] = 0;
+            win_coordinates[1] = 0;
         }
-        else if (board[0][2] == 'x' && board[1][1] == 'x' && board[2][0] == '') {
-            block = true;
-            block_coordinates[0] = 2;
-            block_coordinates[1] = 0;
+        else if (board[0][2] == 'o' && board[1][1] == 'o' && board[2][0] == '') {
+            win = true;
+            win_coordinates[0] = 2;
+            win_coordinates[1] = 0;
         }
-        else if (board[0][2] == 'x' && board[1][1] == '' && board[2][0] == 'x') {
-            block = true;
-            block_coordinates[0] = 1;
-            block_coordinates[1] = 1;
+        else if (board[0][2] == 'o' && board[1][1] == '' && board[2][0] == 'o') {
+            win = true;
+            win_coordinates[0] = 1;
+            win_coordinates[1] = 1;
         }
-        else if (board[0][2] == '' && board[1][1] == 'x' && board[2][0] == 'x') {
-            block = true;
-            block_coordinates[0] = 0;
-            block_coordinates[1] = 2;
+        else if (board[0][2] == '' && board[1][1] == 'o' && board[2][0] == 'o') {
+            win = true;
+            win_coordinates[0] = 0;
+            win_coordinates[1] = 2;
         }
 
-        if (block) {
-            console.log("Block move")
-            move[0] = block_coordinates[0];
-            move[1] = block_coordinates[1];
+        if (win) {
+            console.log("Win move")
+            move[0] = win_coordinates[0];
+            move[1] = win_coordinates[1];
         }
         else {
-            // The AI:s move will be in one of the corners if possible
-            // Check for free corners
-            if (board[0][0] == '') {
-                move[0] = 0;
-                move[1] = 0;
+            // Check if the oponent needs to be blocked
+            // Check rows
+            for (let i = 0; i < 3; i++) {
+                if (board[i][0] == 'x' && board[i][1] == 'x' && board[i][2] == '') {
+                    block = true;
+                    block_coordinates[0] = i;
+                    block_coordinates[1] = 2;
+                    break;
+                }
+                else if (board[i][0] == 'x' && board[i][1] == '' && board[i][2] == 'x') {
+                    block = true;
+                    block_coordinates[0] = i;
+                    block_coordinates[1] = 1;
+                    break;
+                }
+                else if (board[i][0] == '' && board[i][1] == 'x' && board[i][2] == 'x') {
+                    block = true;
+                    block_coordinates[0] = i;
+                    block_coordinates[1] = 0;
+                    break;
+                }
             }
-            else if (board[0][2] == '') {
-                move[0] = 0;
-                move[1] = 2;
+            // Check columns
+            for (let i = 0; i < 3; i++) {
+                if (board[0][i] == 'x' && board[1][i] == 'x' && board[2][i] == '') {
+                    block = true;
+                    block_coordinates[0] = 2;
+                    block_coordinates[1] = i;
+                    break;
+                }
+                else if (board[0][i] == 'x' && board[1][i] == '' && board[2][i] == 'x') {
+                    block = true;
+                    block_coordinates[0] = 1;
+                    block_coordinates[1] = i;
+                    break;
+                }
+                else if (board[0][i] == '' && board[1][i] == 'x' && board[2][i] == 'x') {
+                    block = true;
+                    block_coordinates[0] = 0;
+                    block_coordinates[1] = i;
+                    break;
+                }
             }
-            else if (board[2][0] == '') {
-                move[0] = 2;
-                move[1] = 0;
+            // Check vertical rows
+            if (board[0][0] == 'x' && board[1][1] == 'x' && board[2][2] == '') {
+                block = true;
+                block_coordinates[0] = 2;
+                block_coordinates[1] = 2;
             }
-            else if (board[2][2] == '') {
-                move[0] = 2;
-                move[1] = 2;
+            else if (board[0][0] == 'x' && board[1][1] == '' && board[2][2] == 'x') {
+                block = true;
+                block_coordinates[0] = 1;
+                block_coordinates[1] = 1;
             }
-            // Check if the middle square is free if no corner is free
-            else if (board[1][1] == '') {
-                move[0] = 1;
-                move[1] = 1;
+            else if (board[0][0] == '' && board[1][1] == 'x' && board[2][2] == 'x') {
+                block = true;
+                block_coordinates[0] = 0;
+                block_coordinates[1] = 0;
+            }
+            else if (board[0][2] == 'x' && board[1][1] == 'x' && board[2][0] == '') {
+                block = true;
+                block_coordinates[0] = 2;
+                block_coordinates[1] = 0;
+            }
+            else if (board[0][2] == 'x' && board[1][1] == '' && board[2][0] == 'x') {
+                block = true;
+                block_coordinates[0] = 1;
+                block_coordinates[1] = 1;
+            }
+            else if (board[0][2] == '' && board[1][1] == 'x' && board[2][0] == 'x') {
+                block = true;
+                block_coordinates[0] = 0;
+                block_coordinates[1] = 2;
+            }
+
+            if (block) {
+                console.log("Block move")
+                move[0] = block_coordinates[0];
+                move[1] = block_coordinates[1];
             }
             else {
-                console.log("Can not find any")
-                move = easy(board);
+                // The AI:s move will be in one of the corners if possible
+                // Check for free corners
+                if (board[0][0] == '') {
+                    move[0] = 0;
+                    move[1] = 0;
+                }
+                else if (board[0][2] == '') {
+                    move[0] = 0;
+                    move[1] = 2;
+                }
+                else if (board[2][0] == '') {
+                    move[0] = 2;
+                    move[1] = 0;
+                }
+                else if (board[2][2] == '') {
+                    move[0] = 2;
+                    move[1] = 2;
+                }
+                // Check if the middle square is free if no corner is free
+                else if (board[1][1] == '') {
+                    move[0] = 1;
+                    move[1] = 1;
+                }
+                else {
+                    console.log("Can not find any")
+                    move = easy(board);
+                }
+                console.log("Corner move")
             }
-            console.log("Corner move")
         }
     }
+    console.log(move);
     makeMove(board, move);
 }
 
@@ -267,7 +296,7 @@ function decideMoveDependingOnDifficulty(board) {
 
 function getPlayerInfo() {
     let selectedRadioButton = document.getElementsByName("difficulty");
-    playerName = document.getElementsByName("name")[0].value;
+    playerInfo.playerName = document.getElementsByName("name")[0].value;
     for (let i = 0; i < 3; i++) {
         if (selectedRadioButton[i].checked) {
             board[3][0] = selectedRadioButton[i].value;
@@ -277,12 +306,68 @@ function getPlayerInfo() {
     console.log(board[3][0]);
 }
 
-let playerName = "";
+function checkForWin(board) {
+    let win = undefined;
+    let players = ['x', 'o'];
+    outer: for (player of players) {
+        for (let i = 0; i < 3; i++) {
+            if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
+                win = player;
+                break outer;
+            }
+            else if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
+                win = player;
+                break outer;
+            }
+            else if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
+                win = player;
+                break outer;
+            }
+            else if (board[0][i] == player && board[1][i] == player && board[2][i] == player) {
+                win = player;
+                break outer;
+            }
+            else if (board[0][i] == player && board[1][i] == player && board[2][i] == player) {
+                win = player;
+                break outer;
+            }
+            else if (board[0][i] == player && board[1][i] == player && board[2][i] == player) {
+                win = player;
+                break outer;
+            }
+        }
+        if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+            win = player;
+            break outer;
+        }
+        else if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+            win = player;
+            break outer;
+        }
+        else if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+            win = player;
+            break outer;
+        }
+        else if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
+            win = player;
+            break outer;
+        }
+        else if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
+            win = player;
+            break outer;
+        }
+        else if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
+            win = player;
+            break outer;
+        }
+    }
+    return win;
+}
+
+let playerInfo = {playerName:"", wins:0, losses:0, draws:0};
+
 let board = [['', '', ''],
              ['', '', ''],
              ['', '', ''], ["", "easy"]];
-let wins = 0;
-let losses = 0;
-let draws = 0;
 
 console.log(hard(board));
